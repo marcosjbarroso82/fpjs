6c5f6b2f654154b6bdc4ed4b5fcd75dba5c414ae
Marcos Javier Barroso <marcosjbarroso82@gmail.com>
2025-01-04 20:48:50 -0300

    Refactor App structure and add todo functionality
    - Removed the old App.js file and integrated its functionality into index.js.
    - Implemented a new app function that initializes a todo model and renders a todo table.
    - Added a new style.css file for basic styling of the todo table.
    - Removed the unused tree.js module to clean up the codebase.

--- tree_framework/src/index.js
+++ tree_framework/src/index.js
@@ -1,6 +1,53 @@
-import { tree } from './tree'
-import { app } from './App'
+import './style.css';
+import { h, diff, patch } from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
+import hh from 'hyperscript-helpers';
+
+const { div, h1, table, tr, td, th } = hh(h);
+
+function update(msg, model) {
+  return model;
+}
+
+function view(model) {
+  const todoRows = model.todos.map(todo => {
+    return tr({}, [
+      td({}, [
+        todo.text
+      ])
+    ])
+  })
+
+  const todoTable = table({}, [
+    todoRows
+  ])
+  return todoTable;
+}
+
+function app(initialData, node) {
+  let model = update('INIT', initialData);
+  let content = view(model);
+  let rootNode = createElement(content);
+  node.appendChild(rootNode);
+  console.log(rootNode);
+}
 
 const node = document.getElementById('app');
 
-app(tree, node);
+const initialData = {
+  todos: [
+    {
+      id: 1,
+      text: 'Learn React',
+      completed: false
+    },
+    {
+      id: 2,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ],
+  nextTodoId: 3
+}
+
+app(initialData, node);
