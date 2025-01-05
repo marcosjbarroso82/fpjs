8b30bbcb3c57a2d76a36f5c39d44ce5223ce02b5
Marcos Javier Barroso <marcosjbarroso82@gmail.com>
2025-01-05 10:36:19 -0300

    cp

--- tree_framework/src/index.js
+++ tree_framework/src/index.js
@@ -3,51 +3,48 @@ import { h, diff, patch } from 'virtual-dom';
 import createElement from 'virtual-dom/create-element';
 import hh from 'hyperscript-helpers';
 
-const { div, h1, table, tr, td, th } = hh(h);
+const { div, h1, table, tr, td, th, button, input, pre } = hh(h);
 
-function update(msg, model) {
-  return model;
+const initialData = {
+    stock: {
+        items: [
+            {id: 1, name: 'Apple', price: 100, quantity: 100},
+            {id: 2, name: 'Orange', price: 100, quantity: 100},
+            {id: 3, name: 'Banana', price: 100, quantity: 100},
+        ],
+        nextId: 4
+    }
 }
 
-function view(model) {
-  const todoRows = model.todos.map(todo => {
-    return tr({}, [
-      td({}, [
-        todo.text
-      ])
-    ])
-  })
+function update(msg, model) {
+    return model
+}
 
-  const todoTable = table({}, [
-    todoRows
-  ])
-  return todoTable;
+function view(dispatch, model) {
+    return div({}, [
+        h1('Test'),
+        pre(JSON.stringify(model, null, 2))
+    ])
 }
 
 function app(initialData, node) {
-  let model = update('INIT', initialData);
-  let content = view(model);
-  let rootNode = createElement(content);
-  node.appendChild(rootNode);
-  console.log(rootNode);
+    let model = initialData;
+    model = update('INIT', model);
+    let currentView = view(dispatch, model);
+    let rootNode = createElement(currentView);
+    node.appendChild(rootNode);
+
+  function dispatch(msg) {
+    console.log('dispatch', msg);
+    data = update(msg, data);
+    const updatedView = view(dispatch, data);
+    const patches = diff(currentView, updatedView);
+    rootNode = patch(rootNode, patches);
+    currentView = updatedView;
+  } 
 }
 
 const node = document.getElementById('app');
 
-const initialData = {
-  todos: [
-    {
-      id: 1,
-      text: 'Learn React',
-      completed: false
-    },
-    {
-      id: 2,
-      text: 'Learn Redux',
-      completed: false
-    }
-  ],
-  nextTodoId: 3
-}
 
 app(initialData, node);
