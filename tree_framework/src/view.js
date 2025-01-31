d4d8e1442a049099956c238a6205dac05e14d981
Marcos Javier Barroso <marcosjbarroso82@gmail.com>
2025-01-31 09:39:27 -0300

    Add TODO list to debug view for future feature tracking
    - Extended renderDebug function with a list of potential features and improvements
    - Added hyperscript-helpers import for ul and li elements
    - Included a comprehensive list of planned features and enhancements
    - Improved debug view with a clear roadmap of future development tasks

--- tree_framework/src/view.js
+++ tree_framework/src/view.js
@@ -3,7 +3,7 @@ import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 import { renderAnotherCounterForm, renderCounterForm } from './counter/renderCounter';
 
-const { div, h1, h2, p, table, tr, td, th, button, input, pre, hr, label, br } = hh(h);
+const { div, h1, h2, p, table, tr, td, th, button, input, pre, hr, label, br, ul, li } = hh(h);
 export function renderDebug(dispatch, appState) {
     return div({}, [
         h2('Debug'),
@@ -12,7 +12,23 @@ export function renderDebug(dispatch, appState) {
         pre(JSON.stringify(appState.nextMsg, null, 2)),
         button({ onclick: () => dispatch({ type: EXECUTE_NEXT_MSG }) }, 'Execute Next Msg'),
         h2('Message Dispatcher'),
-        p('To be implemented')
+        p('To be implemented'),
+        ul(
+            [
+                li('Independent Counter'),
+                li('Profile'),
+                li('Duplicated Profile'),
+                li('Independent Profile'),
+                li('Toggle Automatic Execution'),
+                li('Pause/Play'),
+                li('Send Message'),
+                li('Rewind / Forward'),
+                li('Send Message from outside the app'),
+                li('Implement Time from outside the app'),
+                li('Implement Validation of Inputs and Outputs with JSON Schema or other'),
+                li('Display TODOs and Documentation from a Markdown file'),
+            ]
+        )
     ]);
 }// Renders
 export function view(dispatch, appState) {
